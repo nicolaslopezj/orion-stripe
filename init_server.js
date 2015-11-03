a2da199da74a8d698ca2cf6a0b56e47107c771de
Nicolás López <nicolaslopezj@me.com>
2015-11-03 19:47:37 -0300

    add indexes

--- init_server.js
+++ init_server.js
@@ -1,3 +1,17 @@
+Meteor.startup(function () {
+  orion.stripe.cards._ensureIndex({
+    'metadata.userId': 1,
+    id: 1,
+    customer: 1
+  });
+
+  orion.stripe.charges._ensureIndex({
+    'metadata.userId': 1,
+    id: 1,
+    created: 1
+  });
+});
+
 Object.defineProperty(orion.stripe, 'api', {
   get: function() {
     return StripeSync(orion.config.get('STRIPE_SECRET_KEY'));
